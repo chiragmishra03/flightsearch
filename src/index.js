fe2a0e06905de20c8f0a0620d33ebc84c02d8197
CHIRAG MISHRA <chirag3102001@gmail.com>
2023-11-10 01:47:50 +0530

    setting up index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 dotenv.config();
 const PORT = process.env.PORT;
 const setupAndStart = async () => {
   const app = express();
+  app.use(bodyParser);
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
     console.log();
     console.log(`Server started at ${PORT}`);
